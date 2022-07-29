0de9d6885ea53c5cf09dd80f25e7277ab94e38c3
kapilsapkota <searchin.kapil@gmail.com>
2022-07-29 22:37:04 +0545

    Added Copy

--- js/external.js
+++ js/external.js
@@ -1,7 +1,7 @@
 var a=1;
 	var b=2;
 	var c = a + b;
-	
+	console.log(c)
 	function getDate(){
 		var date = new Date();
 		var par = document.getElementById("current_date");
@@ -9,3 +9,16 @@ var a=1;
 		par.innerHTML = date;
 		par.style.color = "red";
 	}
+	//getting Email From Form
+	 function getEmail(){
+	 	var email = document.getElementById("email").value;
+	 	var name = document.getElementById("user_name").value;
+	 	var password = document.getElementById("password").value;
+	 	var role = document.getElementById("role").value;
+	 	var remarks = document.getElementById("remarks").value;
+	 	document.getElementById("getEmail").innerHTML = 
+	 	"Your email is:<b>" +email+ 
+	 	"</b><br> Your UserName is <b>"+ name+
+	 	 "</b> <br> You type password:<b>" + password +
+	 	 "</b> <br> You select role: </b>"+ role+"</b>";
+	 }
